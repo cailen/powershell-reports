4a93c5100f78775edd3261bd50c5727c86755e4d
Derek Wiedenhoeft <derek@derekdesignsorlando.com>
2016-11-17 03:03:29 -0500

    All initital variables setup and working.

--- HyperV Powershell/proxmox.ps1
+++ HyperV Powershell/proxmox.ps1
@@ -22,23 +22,81 @@ $cookie.Domain = $servername
 $session.Cookies.Add($cookie);
 #=================================================
 
-$nodes = Invoke-RestMethod -uri ($uri+'nodes/') -WebSession $session -Verbose
+#region ::: Variables
+$TotalHddMax = 0.00
+$TotalHddCurrent = 0.00
+$TotalHddOnline = 0.00
+$TotalRamMax = 0.00
+$TotalRamCurrent = 0.00
+$TotalRamOnline = 0.00
+$driveArrCurrent = New-Object 'System.Collections.Generic.Dictionary[string,double]'
+$driveArrOnline = New-Object 'System.Collections.Generic.Dictionary[string,double]'
+$driveArrMax = New-Object 'System.Collections.Generic.Dictionary[string,double]'
+$driveArrUsed = @{}
+$driveArrFree = @{}
+$hddContainer = "info"
+$hddUsedProgressBar = "success"
+$vhd_usedPB = ""
+$vhd_table = ""
+$vms_string = ""
+$hddMaxPercent = 0.00
+$hddOnlinePercent = 0.00
+$hddCurrentPercent = 0.00
+$harddriveString = ""
 
+$reportTitle = "Proxmox Report for $($nodes.data[0].node)"
+$reportDate = "$(Get-Date -Format "MM-dd-yyyy")"
+
+$nodes = Invoke-RestMethod -uri ($uri+'nodes/') -WebSession $session -Verbose
 foreach ($node in $nodes.data) {
     $qemus = Invoke-RestMethod -uri ($uri+'nodes/'+$node.node+'/qemu') -WebSession $session -Verbose
-    foreach ($qemu in $qemus.data) {
-        #will need to popular the {storage} name since it won't always be local
-        $content = Invoke-RestMethod -uri ($uri+'nodes/'+$node.node+'/storage/local/content') -WebSession $session -Verbose
-        foreach ($item in $content.data) {
-            if ($item.content -eq "images") {
-                if($item.vmid -eq $qemu.vmid)
-                {
-                    $item
-                }
-            }
-        }
+    $lxcs = Invoke-RestMethod -uri ($uri+'nodes/'+$node.node+'/lxc') -WebSession $session -Verbose
+    $storages = Invoke-RestMethod -uri ($uri+'nodes/'+$node.node+'/storage') -WebSession $session -Verbose
+    foreach ($storage in $storages.data) {
+        $content = Invoke-RestMethod -uri ($uri+'nodes/'+$node.node+'/storage/'+$storage.storage+'/content') -WebSession $session -Verbose
     }
 }
+
+$HostMemory = $nodes.data | Select maxmem
+$HostProcessors = $nodes.data | Select maxcpu
+$Disks = $content.data | Where content -eq "images" | Select vmid, size, used | Sort vmid
+$Storages = $storages.data | Select storage, total, used, avail #$driveLetters = gdr -PSProvider 'FileSystem' | Select Name, Used, Free 
+
+$VMs = $qemus.data | Select vmid, name, status, mem, maxmem, cpus | Sort vmid
+$LXCs = $lxcs.data | Select vmid, name, status, mem, maxmem, cpus | Sort vmid
+#<-----VMs---------->
+$runningVMs = ($VMs | Where status -eq "running" | Measure).Count
+$pausedVMs = ($VMs | Where {($_.status -eq "paused") -or ($_.status -eq "saved")} | Measure).Count
+$offlineVMs = ($VMs | Where status -eq "stopped" | Measure).Count
+$totalVMs = ($VMs).Count
+#<-----Containers--->
+$runningLXCs = ($LXCs | Where status -eq "running" | Measure).Count
+$pausedLXCs = ($LXCs | Where {($_.status -eq "paused") -or ($_.status -eq "saved")} | Measure).Count
+$offlineLXCs = ($LXCs | Where status -eq "stopped" | Measure).Count
+$totalLXCs = ($LXCs).Count
+
+#endregion ::: Variables
+
+#foreach ($node in $nodes.data) {
+#    $qemus = Invoke-RestMethod -uri ($uri+'nodes/'+$node.node+'/qemu') -WebSession $session -Verbose
+#    foreach ($qemu in $qemus.data) {
+#        #will need to populate the {storage} name since it won't always be local
+#        $content = Invoke-RestMethod -uri ($uri+'nodes/'+$node.node+'/storage/local/content') -WebSession $session -Verbose
+#        foreach ($item in $content.data) {
+#            if ($item.content -eq "images") {
+#                if($item.vmid -eq $qemu.vmid)
+#                {
+#                    $disk = $item
+#                    $disk
+#                }
+#            }
+#        }
+#    }
+#}
     
+#$test = Invoke-RestMethod -uri ($uri+'nodes/') -WebSession $session
+#$test.data
+
+$storages
 
 Remove-Variable session
