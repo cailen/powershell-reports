edb32761e5071e0f05c0507aeb1395fcffa12ff6
Derek Wiedenhoeft <derek@derekdesignsorlando.com>
2016-11-17 07:37:20 -0500

    POW-3 Removed test passwords and set test to active for testing

--- HyperV Powershell/proxmox.ps1
+++ HyperV Powershell/proxmox.ps1
@@ -1,17 +1,12 @@
 ﻿[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
 
 $servername = Read-Host -Prompt 'What is the server IP?'
-#$servername = "209.208.48.68"
-
 $uri = 'https://'+$servername+':8006/api2/json/'
-#$uri = 'https://209.208.48.68:8006/api2/json/'
-
 $ticketuri = $uri+'access/ticket'
 $C = Get-Credential -Message 'Enter the server login'
 
 #==========Authenticate with the Server===========
 $ticket = Invoke-RestMethod -Method Post -uri $ticketuri -body ('username='+$C.UserName+'@pam&password='+$C.GetNetworkCredential().Password) -Verbose
-#$ticket = Invoke-RestMethod -Method Post -uri $ticketuri -body ('username=root@pam&password=Nrprocks!') -Verbose
 
 $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
 $cookie = New-Object System.Net.Cookie    
